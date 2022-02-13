6cebd8f79fdf0975854d19247a73f68e3376a6bc
Zhonghe Feng <gref.90@gmail.com>
2022-02-13 14:58:32 +0000

    Read answer dictionary

--- helper.py
+++ helper.py
@@ -31,8 +31,18 @@ def get_guess_dictionary() -> list:
     return dictionary
 
 
+def get_answer_dictionary() -> list:
+    with open('words_wordle_solutions.txt', 'r') as f:
+        dictionary = f.readlines()
+    for i in range(len(dictionary)):
+        # print(len(word))
+        assert len(dictionary[i]) == 6
+        dictionary[i] = dictionary[i][:5]
+    return dictionary
+
+
 if __name__ == "__main__":
-    guess_dict = get_guess_dictionary()
+    guess_dict = get_answer_dictionary()
     print(len(guess_dict))
 
 #EOF
